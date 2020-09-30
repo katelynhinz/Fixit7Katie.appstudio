bec4b128ceebc0293aaa828257ced95777b07d8f
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-09-30 14:58:54 -0500

    Create while.js

---
+++ forms/while/while.js
@@ -0,0 +1,10 @@
+let numberOfIterations = prompt("How many times would you like this to run?")
+let i = 0
+while(i<= numberOfIterations) {
+
+  console.log(`This is loop iteration ${i}.`);
+
+  i++;
+  }
+
+console.log("The loop is now done.")
